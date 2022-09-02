19532750020fb45a3289edf9a78bcce713bd36a2
Andrey Nikitin <andrey.asdf2014@gmail.com>
2022-09-02 22:59:15 +0200

    Setting up Camera

--- Source/ShooterDemo/ShooterCharacter.cpp
+++ Source/ShooterDemo/ShooterCharacter.cpp
@@ -3,12 +3,26 @@
 
 #include "ShooterCharacter.h"
 
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
 // Sets default values
 AShooterCharacter::AShooterCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	//Attach SpringArm; pulls in towards a character if there is a collision
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 300.f; //arm length / camera distance
+	CameraBoom->bUsePawnControlRotation = true; //rotate the arm based on the controller
+
+	//Attach Camera to SpringArm
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false; //camera does not rotate when we rotate arm
+		
 }
 
 // Called when the game starts or when spawned
