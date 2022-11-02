9331443e415d2f9151949374036a84cb1e90ee2e
Andrey Nikitin <andrey.asdf2014@gmail.com>
2022-11-03 00:51:23 +0100

    explosive

---
+++ Source/ShooterDemo/Explosive.h
@@ -0,0 +1,38 @@
+// Andrei Nikitin 2022
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BulletHitInterface.h"
+#include "GameFramework/Actor.h"
+#include "Explosive.generated.h"
+
+UCLASS()
+class SHOOTERDEMO_API AExplosive : public AActor, public IBulletHitInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AExplosive();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+
+	//Explosion when hit by a bullet
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat", meta = (AllowPrivateAccess = "true"))
+	UParticleSystem* ExplodeParticles;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat", meta = (AllowPrivateAccess = "true"))
+	class USoundCue* ImpactSound;
+	
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void BulletHit_Implementation(FHitResult HitResult) override;
+
+};
