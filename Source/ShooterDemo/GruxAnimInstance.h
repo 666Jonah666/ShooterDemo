281e399362bc9e24c4b7383303360a0741851434
Andrey Nikitin <andrey.asdf2014@gmail.com>
2022-11-08 21:39:15 +0100

    grux run anim

--- Source/ShooterDemo/GruxAnimInstance.h
+++ Source/ShooterDemo/GruxAnimInstance.h
@@ -13,5 +13,18 @@ UCLASS()
 class SHOOTERDEMO_API UGruxAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
+public:
 	
+	UFUNCTION(BlueprintCallable)
+	void UpdateAnimationProperties(float DeltaTime);
+
+	
+private:
+	//lateral movement speed
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	float Speed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class AEnemy* Enemy;
+
 };
