eef0cb1fe39f4394cff1d4a7bbeae30d4ed5cb15
Andrey Nikitin <andrey.asdf2014@gmail.com>
2022-11-20 22:10:36 +0100

    post process effects

--- Source/ShooterDemo/ShooterCharacter.h
+++ Source/ShooterDemo/ShooterCharacter.h
@@ -467,6 +467,7 @@ private:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat", meta = (AllowPrivateAccess = "true"))
 	UAnimMontage* DeathMontage;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Combat", meta = (AllowPrivateAccess = "true"))
 	bool bDying;
 
 public:
